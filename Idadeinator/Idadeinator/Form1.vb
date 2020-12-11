d9a5d099b8487f9d919bd144df5a99f4b6918ec8
Sup3rPlays <73548496+SPFck@users.noreply.github.com>
2020-12-11 11:17:02 +0000

    Add files via upload

---
+++ Idadeinator/Idadeinator/Form1.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Regex.IsMatch(TextBox1.Text, "^[0-9 ]+$") Then
+            MessageBox.Show("A sua idade é: " + TextBox1.Text)
+        End If
+    End Sub
+End Class
